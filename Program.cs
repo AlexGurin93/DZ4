7877a5c9b1801a5713f4a2f35cf48c1219cd9a11
Alexander <rold_teem_dfc@mail.ru>
2022-08-03 16:49:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// 25 задача
+/*Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
+*/
+// 27 задача
+/*
+Console.WriteLine("введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+*/
+//29 задача
+int size = 8;
+int [] array = new int[size];
+Random rand= new Random ();
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = rand.Next(0,12);
+}
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+for (int i = 0; i < size - 1; i++)
+{
+    int minPosition = i;
+    for ( int j = i + 1; j < size; j ++)
+    {
+        if (array[j]< array[minPosition]) minPosition =j;
+    }
+    int temporary = array[i];
+    array[i] = array [minPosition];
+    array [minPosition] = temporary;
+}
+// как это всё совместить,я не понимаю...
